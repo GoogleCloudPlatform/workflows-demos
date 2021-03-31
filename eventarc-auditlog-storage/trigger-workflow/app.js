3f96eabc02b29bf194c8c96c5e5eeeb3b09536c7
Mete Atamel <atamel@google.com>
2021-03-31 10:06:26 +0200

    Finished Eventarc AuditLog Storage integration sample #4

--- eventarc-auditlog-storage/trigger-workflow/app.js
+++ eventarc-auditlog-storage/trigger-workflow/app.js
@@ -18,6 +18,9 @@ const app = express();
 const {ExecutionsClient} = require('@google-cloud/workflows');
 const client = new ExecutionsClient();
 
+const { HTTP } = require("cloudevents");
+const {toLogEntryData} = require('@google/events/cloud/audit/v1/LogEntryData');
+
 const GOOGLE_CLOUD_PROJECT = process.env.GOOGLE_CLOUD_PROJECT;
 const WORKFLOW_REGION = process.env.WORKFLOW_REGION;
 const WORKFLOW_NAME = process.env.WORKFLOW_NAME;
@@ -25,16 +28,24 @@ const WORKFLOW_NAME = process.env.WORKFLOW_NAME;
 app.use(express.json());
 app.post('/', async (req, res) => {
 
-  console.log('Request received:');
-  delete req.headers.Authorization; // do not log authorization header
-  console.log({headers: req.headers, body: req.body});
+  const cloudEvent = HTTP.toEvent({ headers: req.headers, body: req.body });
+  console.log('Received CloudEvent:');
+  console.log(cloudEvent);
+
+  //"protoPayload" : {"resourceName":"projects/_/buckets/events-atamel-images-input/objects/atamel.jpg}";
+  const logEntryData = toLogEntryData(cloudEvent.data);
+  console.log(logEntryData);
+
+  const tokens = logEntryData.protoPayload.resourceName.split('/');
+  const bucket = tokens[3];
+  const file = tokens[5];
 
   try {
     console.log(`Workflow path: ${GOOGLE_CLOUD_PROJECT}, ${WORKFLOW_REGION}, ${WORKFLOW_NAME}`);
     const execResponse = await client.createExecution({
       parent: client.workflowPath(GOOGLE_CLOUD_PROJECT, WORKFLOW_REGION, WORKFLOW_NAME),
       execution: {
-        argument: JSON.stringify({bucket: "foo", file: "bar"})
+        argument: JSON.stringify({bucket: bucket, file: file})
       }
     });
     console.log(`Execution response: ${JSON.stringify(execResponse)}`);
