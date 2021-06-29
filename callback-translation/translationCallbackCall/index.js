15f88037e3c5361457a59e8bbfa2b7cc0f9ad2f7
Camie Kim <contact.camie@gmail.com>
2021-06-29 17:49:12 -0400

    docs: Add region tags for snippet tracking

--- callback-translation/translationCallbackCall/index.js
+++ callback-translation/translationCallbackCall/index.js
@@ -11,7 +11,7 @@
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
-
+// [START workflows_callback_translation]
 const cors = require('cors')({origin: true});
 const fetch = require('node-fetch');
 
@@ -51,3 +51,4 @@ exports.translationCallbackCall = async (req, res) => {
     }
   });
 };
+// [END workflows_callback_translation]
