5d336ccf1ece792ac58d0116e5d7ec0e037f41be
averikitsch <akitsch@google.com>
2022-05-09 13:17:53 -0700

    lint

--- service-chaining/multiply/main.py
+++ service-chaining/multiply/main.py
@@ -20,6 +20,6 @@ from flask import jsonify
 @functions_framework.http
 def multiply(request):
     request_json = request.get_json()
-    output = {"multiplied":2*request_json['input']}
+    output = {"multiplied": 2 * request_json['input']}
     return jsonify(output)
 # [END workflows_functions_multiply]
